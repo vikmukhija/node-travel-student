f006f5e162377dbe621f9f719f58049d74e5c879
vikmukhija <vikmukhija@yahoo.com>
2014-01-12 18:27:31 -0500

    initial commit

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ appHelper.runApp(function(app, db) {
 	console.log("My app is running!");
 
 	app.get("/hello", function (req, resp) {
-		resp.write("HELLO WORLD!");
+		resp.write("HELLO WORLD! My name is Vik!");
 		resp.end();
 	});
-})
+
+	app.get("/html", function (req, resp) {
+		resp.write("<html><body><h1>Node is cool!</h1></body></html>");
+		resp.end();
+	});
+
+	app.get("/", function (req, resp) {
+
+		db.findArray({}, function(arr) {
+			resp.render("index", { locations: arr });
+		});
+});
